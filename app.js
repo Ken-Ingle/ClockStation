4673d43a7827f110519d7cb3216f5116dd508c1d
Ken Ingle <ken@keningle.net>
2022-06-22 11:56:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const hourHand = document.getElementById('hourHandDisplay');
+const minuteHand = document.getElementById('minuteHandDisplay');
+const secondHand = document.getElementById('secondHandDisplay');
+const clockInButton = document.getElementById('clockInButton');
+const clockOutButton = document.getElementById('clockOutButton');
+const shiftsDisplay = document.getElementById('shiftsDisplay');
